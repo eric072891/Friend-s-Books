8a67b7452b195fb139fa7aa4780195a246fe64de
eric072891 <eric072891@gmail.com>
2020-08-13 09:58:35 -0700

    Delete urls.py

--- romanbooks/bookviewer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.categories, name='categories'),
-    path('<cat>/', views.book_category, name='book_category'),
-    path('<cat>/<title>/', views.detail, name='detail'),
-]
